e89bba59191790776f9d2b0a11273d2bcc7b1650
AxelNAY <axelnay@sfr.fr>
2024-03-25 10:09:03 +0000

    add prototype of print_func.c in main.h

--- main.h
+++ main.h
@@ -13,5 +13,16 @@ typedef struct prt
 } prt_t;
 
 int _printf(const char *format, ...);
+void print_length(va_list ap);
+void print_neg(va_list ap);
+void print_uns(va_list ap);
+void print_uns_oct(va_list ap);
+void print_uns_hexa(va_list ap);
+void print_char(va_list ap);
+void print_string(va_list ap);
+void print_addr(va_list ap);
+void print_perc(va_list ap);
+void print_len(va_list ap);
+void print_unknown(va_list ap);
 
 #endif
