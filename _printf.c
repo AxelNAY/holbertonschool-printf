a40f729ed5810f31b2721f9cdefe88a800f1865d
Adrybest <adryanbestard@gmail.com>
2024-03-25 13:27:12 +0100

    Creation of printf

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include "print_func.c"
+/**
+* _printf - print everithing
+* @format: format.
+* @...: arguments
+* Return: len
+*/
+int _printf(const char *format, ...)
+{
+	return (0);
+}
