082c51747085211cfdf2d5649bc4bc2a287eb1ed
Adrybest <adryanbestard@gmail.com>
2024-03-26 10:02:40 +0100

    creation of _printf.c of all things we need it

--- _printf.c
+++ _printf.c
@@ -11,26 +11,40 @@ int _printf(const char *format, ...)
 	va_list ap;
 	int i = 0;
 	int j = 0;
-	char *str;
 
-	if (str == NULL)
+	prt_t type[] = {
+	{"c", print_char},
+	{"s", print_string},
+	{'%', print_perc},
+	{"d", print_int},
+	{"i", print_int}
+	};
+
+	if (format == NULL)
 	{
 		return (-1);
 	}
-	va_start(ap,format);
-
-	prt_t type;
+	va_start(ap, format);
 
 	while (format[i])
 	{
-		str[i] = format[i];
+		if (format[i] == '%')
+		{
+			for (j = 0; type[j].pr; j++)
+			{
+				if (type[j].pr == format[i])
+				{
+				type[j].f(ap);
+				i = i + 2;
+				}
+			}
+		}
+		else if (format[i] == '\\' && format[i++] == 'n')
+		{
+			_putchar('\n');
+			i = i + 2;
+		}
+		else
 		_putchar(format[i]);
 	}
-	for (j = 0; type[j].pr; j++)
-	if (type[j].pr == str)
-	_putchar(':');
-	_putchar('[');
-	type[j].f(ap);
-	_putchar(']');
-	_putchar('\n');
 }
