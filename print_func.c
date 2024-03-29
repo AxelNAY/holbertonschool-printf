ff734f7d5608432ce59b935c2e147c6fa746c245
Adrybest <adryanbestard@gmail.com>
2024-03-29 15:36:08 +0100

    modification in print_func

--- print_func.c
+++ print_func.c
@@ -12,7 +12,7 @@ int print_int(va_list ap)
 	char buffer[22], *str;
 
 	str = itoa(va_arg(ap, int), buffer, 10);
-	for (i = 0; i < str[i]; i++)
+	for (i = 0; str[i]; i++)
 		_putchar(str[i]);
 
 	return (i);
